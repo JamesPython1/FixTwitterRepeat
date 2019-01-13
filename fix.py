d9d2c2ffcf7a0eb6b453cdf66bcb0c6c78df889c
JamesPython1 <46634565+JamesPython1@users.noreply.github.com>
2019-01-13 09:38:41 +0000

    Update fix.py
    Just made it more reliable

--- fix.py
+++ fix.py
@@ -13,12 +13,14 @@ twitter = Twython(
     access_token,
     access_token_secret,
 )
-
 listofnum = list(range(1,1000000))
 numb = random.choice(listofnum)
 print(numb)
-fg = str(numb)
 listofnum.remove(numb)
+
+fg = str(numb)
 
 message = str(fg + " [PUT YOUR TEXT HERE]")
 twitter.update_status(status=message)
+
+
